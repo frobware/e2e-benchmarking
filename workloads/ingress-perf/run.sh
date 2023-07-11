1fcbc8c2707ef5a7fcb60c517bf75180ab3e665f
Andrew McDermott <aim@frobware.com>
2023-07-11 14:14:00 +0100

    workloads/ingress-perf/config: add baremetal config

--- workloads/ingress-perf/run.sh
+++ workloads/ingress-perf/run.sh
@@ -4,7 +4,7 @@ set -e
 
 ES_SERVER=${ES_SERVER:-https://search-perfscale-dev-chmf5l4sh66lvxbnadi4bznl3a.us-west-2.es.amazonaws.com}
 ES_INDEX=${ES_INDEX:-ingress-performance}
-LOG_LEVEL=${LOG_LEVEL:-info}
+LOG_LEVEL=${LOG_LEVEL:-debug}
 VERSION=${VERSION:-0.2.3}
 CONFIG=${CONFIG:?}
 BASELINE_UUID=${BASELINE_UUID:-}
@@ -13,18 +13,18 @@ TOLERANCY=${TOLERANCY:-20}
 OS=$(uname -s)
 HARDWARE=$(uname -m)
 
-download_binary(){
-  INGRESS_PERF_URL=https://github.com/cloud-bulldozer/ingress-perf/releases/download/v${VERSION}/ingress-perf-${OS}-v${VERSION}-${HARDWARE}.tar.gz
-  curl -sS -L ${INGRESS_PERF_URL} | tar xz ingress-perf
-}
+# download_binary(){
+#   INGRESS_PERF_URL=https://github.com/cloud-bulldozer/ingress-perf/releases/download/v${VERSION}/ingress-perf-Linux-v${VERSION}-x86_64.tar.gz
+#   curl -sS -L ${INGRESS_PERF_URL} | tar xz ingress-perf
+# }
 
-download_binary
+# download_binary
 cmd="./ingress-perf run --cfg ${CONFIG} --loglevel=${LOG_LEVEL}"
 if [[ -n ${ES_SERVER} ]]; then
-  cmd+=" --es-server=${ES_SERVER} --es-index=${ES_INDEX}"
+  cmd+=" --es-server=${ES_SERVER} --es-index=${ES_INDEX} --cleanup=false"
 fi
 if [[ -n ${BASELINE_UUID} ]]; then
-  cmd+=" --baseline-uuid=${BASELINE_UUID} --baseline-index=${BASELINE_INDEX} --tolerancy=${TOLERANCY}"
+  cmd+=" --baseline-uuid=${BASELINE_UUID} --baseline-index=${BASELINE_INDEX} --tolerancy=${TOLERANCY} --cleanup=false"
 fi
 echo $cmd
 exec $cmd
